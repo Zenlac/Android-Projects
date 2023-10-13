364279320be7cf53cd4897f843c5d3b945987c11
Zenlac <caleleizengrillera@gmail.com>
2023-10-13 09:40:28 +0800

    added design but images dont work

--- SaveStraysDashboard/app/src/main/java/com/example/savestraysdashboard/MainActivity.kt
+++ SaveStraysDashboard/app/src/main/java/com/example/savestraysdashboard/MainActivity.kt
@@ -1,46 +1,72 @@
 package com.example.savestraysdashboard
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.savestraysdashboard.ui.theme.SaveStraysDashboardTheme
+import kotlin.collections.Map
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SaveStraysDashboardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val petAdoptionBtn = findViewById<Button>(R.id.PetAdoptionBtn)
+        petAdoptionBtn.setOnClickListener{
+            val intent = Intent(this,PetAdoption::class.java)
+            startActivity(intent)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        val petStoreBtn = findViewById<Button>(R.id.PetStoreBtn)
+        petStoreBtn.setOnClickListener{
+            val intent = Intent(this,PetStore::class.java)
+            startActivity(intent)
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SaveStraysDashboardTheme {
-        Greeting("Android")
+        val strayReportingBtn = findViewById<Button>(R.id.StrayReportingBtn)
+        strayReportingBtn.setOnClickListener{
+            val intent = Intent(this,StrayReporting::class.java)
+            startActivity(intent)
+        }
+
+        val settingsBtn = findViewById<Button>(R.id.SettingsBtn)
+        settingsBtn.setOnClickListener{
+            val intent = Intent(this,Settings::class.java)
+            startActivity(intent)
+        }
+
+        val mapBtn = findViewById<Button>(R.id.MapBtn)
+        mapBtn.setOnClickListener{
+            val intent = Intent(this, Map::class.java)
+            startActivity(intent)
+        }
+
+        val logoutBtn = findViewById<Button>(R.id.LogoutBtn)
+        logoutBtn.setOnClickListener{
+            val intent = Intent(this,Logout::class.java)
+            startActivity(intent)
+        }
+
+        val profileBtn = findViewById<ImageButton>(R.id.ProfileBtn)
+        profileBtn.setOnClickListener{
+            val intent = Intent(this,Profile::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
+
 }
--- SaveStraysDashboard/app/src/main/java/com/example/savestraysdashboard/PetAdoption.kt
+++ SaveStraysDashboard/app/src/main/java/com/example/savestraysdashboard/PetAdoption.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class PetAdoption : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pet_adoption2)
+        setContentView(R.layout.activity_pet_adoption)
     }
 }
