e16e9e98526a85f57f72aa84e105db372c5f10e6
Zenlac <caleleizengrillera@gmail.com>
2023-11-21 16:13:33 +0800

    Start to add functionality

--- SaveStraysDashboard/app/src/main/java/com/example/savestraysdashboard/ReportStray.kt
+++ SaveStraysDashboard/app/src/main/java/com/example/savestraysdashboard/ReportStray.kt
@@ -1,11 +1,33 @@
 package com.example.savestraysdashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 
 class ReportStray : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var btPickImage: Button
+    private lateinit var btTakeImage: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_stray)
+        imageView = findViewById(R.id.image_view)
+        btPickImage = findViewById(R.id.ImagePickerBtn)
+
+        btPickImage.setOnClickListener{
+            pickImage()
+        }
+    }
+    fun pickImage() {
+        val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
+        startActivityForResult(intent,101)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
