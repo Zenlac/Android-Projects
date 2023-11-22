b3d5b16527d91a3c73ebc66fbc93f6a9d4e48cf5
Zenlac <caleleizengrillera@gmail.com>
2023-11-22 14:47:42 +0800

    added camera and image functionalities

--- SaveStraysDashboard/app/src/main/java/com/example/savestraysdashboard/ReportStray.kt
+++ SaveStraysDashboard/app/src/main/java/com/example/savestraysdashboard/ReportStray.kt
@@ -1,6 +1,7 @@
 package com.example.savestraysdashboard
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -17,11 +18,23 @@ class ReportStray : AppCompatActivity() {
         setContentView(R.layout.activity_report_stray)
         imageView = findViewById(R.id.image_view)
         btPickImage = findViewById(R.id.ImagePickerBtn)
+        btTakeImage = findViewById(R.id.ImageTakerBtn)
+
+        btTakeImage.setOnClickListener{
+            takeImage()
+        }
 
         btPickImage.setOnClickListener{
             pickImage()
         }
     }
+    fun takeImage() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent,1)
+    }
+
+
+
     fun pickImage() {
         val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
         startActivityForResult(intent,101)
@@ -29,5 +42,16 @@ class ReportStray : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK) {
+            if(requestCode == 101 && data !=null) {
+                val uri = data?.data
+                imageView.setImageURI(uri)
+
+            }
+            if(requestCode == 1 && data !=null) {
+                val uri = data?.data
+                imageView.setImageBitmap(data.extras!!.get("data") as Bitmap)
+            }
+        }
     }
 }
